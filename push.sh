877dd353667af21680afb106a8e1a16fa1132c06
910318830 <910318830@qq.com>
2023-09-07 12:15:58 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
+git config --global http.sslverify false
 git config --global user.name '910318830'
 git config --global user.email '910318830@qq.com'
 git add .
-git commit -m $1
+git commit -m 自动推送
 git push
